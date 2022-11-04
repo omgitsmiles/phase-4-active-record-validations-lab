c1e7ef4d442d78244b30ffaafbe0c9f249ce5cd4
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-11-04 15:57:46 -0400

    commit tree c0f80fb5a3e24c11c1bd428ce8abad43a32a2bee

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :title, :clickbait?
+
+    BAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+      
+    def clickbait?
+        if BAIT_PATTERNS.none? { |pattern| pattern.match(title) }
+          errors.add(:title, "must be clickbait")
+        end
+      end
+
+
+    # def no_clickbait
+    #     unless title.present? && title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+    #     errors.add(:title, 'Your title is not clickbait-y')
+    #     end
+    # end
 end
+
